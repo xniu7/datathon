fb478335d56c77087fe8102b8348bd2b543d8011
xniu7 <nx.niuxiang@gmail.com>
2016-04-30 16:38:24 -0400

    results

--- results/show_result.py
+++ results/show_result.py
@@ -1,3 +1,6 @@
+import sys
+
+country={'usa':'usa', 'chn':'chn', 'ind':'ind', 'jpn':'jpn', 'deu':'deu', 'gbr':'gbr', 'rus':'rus', 'irq':'irq', 'irn':'irn', 'afg':'afg', 'sau':'sau', 'grc':'grc', 'isl':'isl', 'sun':'rus'}
 
 product_hs={'052709':'Crude Petroleum', '147108':'Gold', '168471':'Computers', '168517':'Telephone Sets', '168528':'TV'}
 
@@ -10,19 +13,37 @@ def generateShowData(rawData, products):
         show_data[year]={}
         current_year = show_data[year]
         id_scores = data[year]
+        
+        for product in products.values():
+            current_year[product]=0
         for (id, score) in id_scores:
             if id not in products.keys(): continue
-            if products[id] not in current_year: current_year[products[id]]=0
+            
             current_year[products[id]]+=score
     return show_data
 
-
-filename="product_results.dat"
+filename= sys.argv[1]
 f = open(filename, 'r')
 for line in f:
     data = eval(line)
-    show_data = generateShowData(data, product_hs)
-#print show_data
-    for year in show_data:
-        current_year = show_data[year]
-        print year, current_year['Crude Petroleum'], current_year['Gold'], current_year['Computers'], current_year['Telephone Sets'], current_year['TV']
+
+    if filename == "recent_product_result.dat" or filename == "old_product_result.dat":
+        if filename == "recent_product_result.dat": products = product_hs
+        elif filename == "old_product_result.dat": products = product_sitc
+        show_data = generateShowData(data, products)
+        #print show_data
+        for year in show_data:
+            current_year = show_data[year]
+            print year, current_year['Crude Petroleum'], current_year['Gold'], current_year['Computers'], current_year['Telephone Sets'], current_year['TV']
+    else :
+        show_data = generateShowData(data, country)
+        #print show_data
+        for year in show_data:
+            current_year = show_data[year]
+            print year, current_year['usa'], current_year['chn'], current_year['ind'], current_year['jpn'], current_year['deu'], current_year['gbr'], current_year['rus'], current_year['irq'], current_year['irn'], current_year['afg'], current_year['sau'], current_year['grc'], current_year['isl']
+
+
+
+
+
+
